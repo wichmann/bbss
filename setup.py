0825dda8373ee59c08ca78215c025f3615c8e6b6
Christian Wichmann <cw1981@gmx.de>
2014-02-14 22:29:40 +0100

    Experimented with setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-try:
-    from setuptools.core import setup
-except ImportError:
-    from distutils.core import setup
+
+from cx_Freeze import setup, Executable
+#try:
+#    from setuptools.core import setup
+#except ImportError:
+#    from distutils.core import setup
+
+
+# Dependencies are automatically detected, but it might need fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+executables = [
+    Executable('bbss_cli.py', 'Console')
+]
 
 setup(
     name='bbss - BBS student management',
-    version='0.0.1',
+    version='0.1',
     author='Christian Wichmann',
     author_email='wichmann@bbs-os-brinkstr.de',
     packages=['bbss'],
@@ -13,4 +23,6 @@ setup(
     license='LICENSE',
     description='Management software for students of german vocational colleges.',
     test_suite='bbss.tests.get_suite'
+    options = dict(build_exe = buildOptions),
+    executables = executables
 )
