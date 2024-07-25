bfc17afcbfbdae303c8a2729ebc5ba844d6a87ca
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2024-07-25 10:57:50 +0200

    Extended log message.

--- bbss/db.py
+++ bbss/db.py
@@ -419,7 +419,7 @@ class StudentDatabase(object):
             logger.debug('\t' + str(s))
             # skip student, if already in list, because that can happen, if students are associated with multiple classes
             if s in change_set.students_changed:
-                logger.warning('Skipping changed student that is already in list!')
+                logger.warning('Skipping changed student that is already in list: {}'.format(s))
                 continue
             change_set.students_changed.append(s)
         change_set.classes_added, change_set.classes_removed = self._get_class_changes(old_import_id, new_import_id)
